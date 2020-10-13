19120d41ae27bd08a049de599205f6abd16ed268
progjs <js568@ajou.ac.kr>
2020-10-14 04:17:33 +0900

    array circle

--- 백준/17144.py
+++ 백준/17144.py
@@ -1,57 +1,63 @@
 # https://www.acmicpc.net/problem/17144
 # 미세먼지 안녕!
 
-R, C, T = map(int, input().split())
-rec, board = [], []
-for i in range(R):
-    board.append(list(map(int, input().split())))
-    if board[-1][0] == -1:
-        rec.append(i)
-
 dr, dc = (0,0,-1,1), (1,-1,0,0)
+
 def spread():
     global R, C, rec, board, board2
-    board2 = [[0]*C for _ in range(R)]
+    board2 = [[0]*C for _ in range(R)] # 미세먼지 확산 후 저장
     
     for r in range(R):
         for c in range(C):
             cnt, dust = 0, board[r][c]//5
-            if board[r][c] > 4:
-                for i in range(4):
+            if board[r][c] >= 5: # 미세먼지 확산 가능
+                for i in range(4): # 상하좌우
                     nr, nc = r+dr[i], c+dc[i]
                     if 0<=nr<R and 0<=nc<C and board[nr][nc] != -1:
                         cnt += 1
                         board2[nr][nc] += dust
-            board2[r][c] = board2[r][c] + board[r][c] - cnt*board[r][c]
-    
+            board2[r][c] = board2[r][c] + board[r][c] - cnt*dust
+
+    # 회전- 반시계
     for i in range(rec[0]-2, -1, -1):
         board2[i+1][0] = board2[i][0]
-    for i in range(1,C):
+    for i in range(1, C):
         board2[0][i-1] = board2[0][i]
     for i in range(1, rec[0]+1):
         board2[i-1][-1] = board2[i][-1]
     for i in range(C-2, 0, -1):
         board2[rec[0]][i+1] = board2[rec[0]][i]
-    board2[rec[0]][1] = 0
-
-    for i in range(rec[1]+2, r):
+    board2[rec[0]][1] = 0 # 공기청정기 출발점 옆 = 0
+    
+    # 회전 - 시계
+    for i in range(rec[1]+2, R):
         board2[i-1][0] = board2[i][0]
-    for i in range(1,C):
+    for i in range(1, C):
         board2[-1][i-1] = board2[-1][i]
     for i in range(R-2, rec[1]-1, -1):
         board2[i+1][-1] = board2[i][-1]
     for i in range(C-2, 0, -1):
         board2[rec[1]][i+1] = board2[rec[1]][i]
-    board2[rec[1]][1] = 0 
+    board2[rec[1]][1] = 0
 
 
-board2 = [[0]*C for _ in range(R)]
-for t in range(T):
-    spread()
-    board = board2.copy()
+if __name__ == "__main__":
+    R, C, T = map(int, input().split())
+    rec, board = [], [] # 공기청정기 위치, 미세먼지 지도
+    for i in range(R):
+        board.append(list(map(int, input().split())))
+        if board[-1][0] == -1:
+            rec.append(i)
 
-total = 0
-for i in range(R):
-    for j in range(C):
-        total += board[i][j]
-print(total+2)
+    board2 = [[0]*C for _ in range(R)]
+    for t in range(T):
+        spread()
+        board = board2.copy() # 초기화
+
+    board[rec[0]][0] = 0
+    board[rec[1]][0] = 0
+    total = 0 # 미세먼지 총합
+    for i in range(R):
+        for j in range(C):
+            total += board[i][j]
+    print(total) # 공기청정기 값 제외
