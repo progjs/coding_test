a46ae4cb190583d18e1f09a448d704d496419f39
progjs <js568@ajou.ac.kr>
2022-03-13 16:10:18 +0900

    dp 정리

--- 백준/1912.py
+++ 백준/1912.py
@@ -7,6 +7,7 @@ input =  stdin.readline
 n = int(input())
 arr = list(map(int,input().split()))
 
+# dp 풀이법     O(n)
 d = [0]*n
 answer = d[0] = arr[0]
 
@@ -16,9 +17,18 @@ for i in range(1,n):
 
 print(answer)    
 
+'''
+DP : 다이나믹 프로그래밍
+중간결과를 저장 -> 속도가 빠름 
+dp[i]를 나타내는 점화식을 찾는것이 중요!
+
+1. dp[0] 초기값을 정하고
+2. dp[i]를 정의
+3. 답을 dp[-1]로 찾거나, dp를 채우면서 답을 찾아내기 -> for문
+'''
 
 # 시간초과
-# n^2~n^3 시간복잡도라서 시간초과....
+# O(n**2)~O(n**3) 시간복잡도라서 시간초과....
 '''
 answer = -10000000
 
